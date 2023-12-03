fa7663403640509aca7e561d8035c7d036179e4c
nijuyonkadesu <24ksriramv@gmail.com>
2023-12-03 16:07:13 +0530

    update telescope to fix unexpected crash

--- lua/24k/packer.lua
+++ lua/24k/packer.lua
@@ -8,7 +8,7 @@ return require('packer').startup(function()
   use 'ThePrimeagen/vim-be-good'
 
   use {
-      'nvim-telescope/telescope.nvim', tag = '0.1.0',
+      'nvim-telescope/telescope.nvim', tag = '0.1.4',
       -- or, branch = '0.1.x',
       requires = { {'nvim-lua/plenary.nvim'} }
   }
