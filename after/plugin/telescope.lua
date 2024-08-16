2f4d103f478cd667b0f10bc0b7f1f78e1a4bb556
nijuyonkadesu <24ksriramv@gmail.com>
2024-08-16 17:46:07 +0530

    telescope: ignore files harpoon: adapt to qwerty

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,11 @@
+require('telescope').setup { 
+  defaults = { 
+    file_ignore_patterns = { 
+      "node_modules", 
+      "venv", 
+    }
+  }
+}
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
