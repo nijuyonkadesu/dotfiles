a8b279b8abe76ae03b2aaf495e9d4b301405af55
nijuyonkadesu <24ksriramv@gmail.com>
2024-11-11 22:51:20 +0530

    fix: update telescope, set highlight color

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -4,6 +4,7 @@ function ColorMyPencils(color)
     
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" }) -- 0 means Global, apply to all windows
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" }) -- apply to floating windows
+    vim.cmd([[hi Search guifg=#191724 guibg=#e0def4]])
 end
 
 ColorMyPencils()
