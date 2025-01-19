0de6156bdc14d15761d3afda30e92c5c30eca6fb
nijuyonkadesu <24ksriramv@gmail.com>
2025-01-19 12:04:21 +0530

    fix: configure and test lua_ls, basedpyright

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -2,10 +2,10 @@ local lsp = require("lsp-zero")
 local cmp = require('cmp')
 local cmp_lsp = require("cmp_nvim_lsp")
 local capabilities = vim.tbl_deep_extend(
-            "force",
-            {},
-            vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities())
+    "force",
+    {},
+    vim.lsp.protocol.make_client_capabilities(),
+    cmp_lsp.default_capabilities())
 require("mason").setup()
 require("mason-lspconfig").setup({
     ensure_installed = {
@@ -15,6 +15,7 @@ require("mason-lspconfig").setup({
         "basedpyright",
         "jsonls",
         "helm_ls",
+        "lua_ls",
     },
     function(server_name) -- default handler (optional)
         require("lspconfig")[server_name].setup {
@@ -37,30 +38,35 @@ local util = require("lspconfig/util")
 --
 -- https://www.reddit.com/r/neovim/comments/17bod01/how_do_i_select_a_python_enviroment_so_pyright/
 -- https://github.com/hahuang65/nvim-config/blob/38aca9f78b4e773d0452ecb953ccdbe9915ac3d9/lua/plugins/lsp.lua#L82
-lspconfig.pyright.setup({
-  root_dir = function(fname)
-    local root = util.root_pattern("requirements.txt", "pyproject.toml", "setup.py", "app")(fname)
-        or util.root_pattern(".git")(fname)
-        or util.path.dirname(fname)
+lspconfig.basedpyright.setup({
+    root_dir = function(fname)
+        local root = util.root_pattern("requirements.txt", "pyproject.toml", "setup.py", "app")(fname)
+            or util.root_pattern(".git")(fname)
+            or util.fs.dirname(fname)
 
-    local venv_dir = util.path.join(root, 'venv')
-    if util.path.exists(venv_dir) then
-      vim.g.python3_host_prog = venv_dir .. '/bin/python'
-    end
+        local venv_dir = root .. 'venv'
+        if util.path.exists(venv_dir) then
+            vim.g.python3_host_prog = venv_dir .. '/bin/python'
+        end
 
-    return root
-  end,
-  settings = {
-    python = {
-      pythonPath = vim.g.python3_host_prog,
-      analysis = {
-        autoSearchPaths = true,
-        useLibraryCodeForTypes = true,
-        diagnosticMode = "workspace",
-        extraPaths = dofile(vim.fn.stdpath('config') .. "/after/plugin/extra-paths.lua"),
-      },
-    }
-  },
+        return root
+    end,
+    settings = {
+        python = {
+            pythonPath = vim.g.python3_host_prog,
+            analysis = {
+                autoSearchPaths = true,
+                useLibraryCodeForTypes = true,
+                diagnosticMode = "workspace",
+                extraPaths = dofile(vim.fn.stdpath('config') .. "/after/plugin/extra-paths.lua"),
+            },
+        },
+        basedpyright = {
+            -- https://github.com/DetachHead/basedpyright/issues/168
+            typeCheckingMode = "standard",
+            reportMissingSuperCall = false,
+        },
+    },
 })
 
 lspconfig.helm_ls.setup {
@@ -87,33 +93,33 @@ lspconfig.lua_ls.setup {
 require("luasnip.loaders.from_vscode").lazy_load()
 local cmp_action = lsp.cmp_action()
 cmp.setup({
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end, 
-  }, 
-  mapping = cmp.mapping.preset.insert({
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-n>'] = cmp.mapping.select_next_item(),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-    ['<CR>'] = cmp.mapping.confirm({
-          behavior = cmp.ConfirmBehavior.Replace,
-          select = true,
+    snippet = {
+        expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+        end,
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+        ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+        ['<CR>'] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
         }),
-    ['<Tab>'] = nil,
-    ['<S-Tab>'] = nil,
+        ['<Tab>'] = nil,
+        ['<S-Tab>'] = nil,
 
-  }),
+    }),
     sources = {
-    { name = "luasnip" }, 
-    { name = "path" },
-    { name = "nvim_lsp" },
-    { name = "buffer", keyword_length = 3 }, 
-    { name = "friendly_snippets" } 
-  },
+        { name = "luasnip" },
+        { name = "path" },
+        { name = "nvim_lsp" },
+        { name = "buffer", keyword_length = 3 },
+        { name = "friendly_snippets" }
+    },
 })
 -- disable completion with tab
 -- this helps with copilot setup
@@ -131,23 +137,23 @@ vim.diagnostic.config({
 })
 
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+    local opts = { buffer = bufnr, remap = false }
 
-  if client.name == "eslint" then
-      vim.cmd.LspStop('eslint')
-      return
-  end
+    if client.name == "eslint" then
+        vim.cmd.LspStop('eslint')
+        return
+    end
 
-  vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-  vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-  vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
-  vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
-  vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
-  vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
-  vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
-  vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
-  vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
-  vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
+    vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
+    vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
+    vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
+    vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
+    vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
 end)
 
 lsp.setup()
@@ -155,4 +161,3 @@ lsp.setup()
 vim.diagnostic.config({
     virtual_text = true,
 })
-
