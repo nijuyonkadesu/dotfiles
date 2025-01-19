42d7c267362cd8b921cf60bdfc240c753030021b
nijuyonkadesu <24ksriramv@gmail.com>
2025-01-19 12:04:21 +0530

    fix: configure and test lua_ls, basedpyright

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,40 +1,53 @@
 return {
     {
-        'nvim-telescope/telescope.nvim', tag = '0.1.8',
-        dependencies = { 
-            { 'nvim-lua/plenary.nvim' } 
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.8',
+        dependencies = {
+            { 'nvim-lua/plenary.nvim' }
         }
     },
-    { 'rose-pine/neovim', name = 'rose-pine' },
+    { 'rose-pine/neovim',                name = 'rose-pine' },
     { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
     { 'ThePrimeagen/harpoon' },
     { 'mbbill/undotree' },
     { 'tpope/vim-fugitive' },
     {
         'towolf/vim-helm',
-        ft = { 'helm' }, 
+        ft = { 'helm' },
         event = { "BufReadPre", "BufNewFile", "BufEnter" }
     },
     {
         'VonHeikemen/lsp-zero.nvim',
         dependencies = {
             -- LSP Support
-            {'neovim/nvim-lspconfig'},
-            {'williamboman/mason.nvim'},
-            {'williamboman/mason-lspconfig.nvim'},
-            {'WhoIsSethDaniel/mason-tool-installer.nvim'},
+            {
+                'neovim/nvim-lspconfig',
+                dependencies = {
+                    "folke/lazydev.nvim",
+                    ft = "lua",
+                    opts = {
+                        library = {
+                            -- Load luvit types when the `vim.uv` word is found
+                            { path = "luvit-meta/library", words = { "vim%.uv" } },
+                        },
+                    },
+                }
+            },
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
+            { 'WhoIsSethDaniel/mason-tool-installer.nvim' },
 
             -- linters, formatters
-            {'nvim-lua/plenary.nvim'},
-            {'jose-elias-alvarez/null-ls.nvim'},
+            { 'nvim-lua/plenary.nvim' },
+            { 'jose-elias-alvarez/null-ls.nvim' },
 
             -- Autocompletion
-            {'hrsh7th/nvim-cmp'},
-            {'hrsh7th/cmp-buffer'},
-            {'hrsh7th/cmp-path'},
-            {'saadparwaiz1/cmp_luasnip'},
-            {'hrsh7th/cmp-nvim-lsp'},
-            {'hrsh7th/cmp-nvim-lua'},
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-path' },
+            { 'saadparwaiz1/cmp_luasnip' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'hrsh7th/cmp-nvim-lua' },
 
             -- Snippets
             {
@@ -42,7 +55,7 @@ return {
                 build = "make install_jsregexp",
                 dependencies = { "rafamadriz/friendly-snippets" },
             },
-            {'rafamadriz/friendly-snippets'},
+            { 'rafamadriz/friendly-snippets' },
         }
     }
 }
