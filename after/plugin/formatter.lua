20ecb82e560850b94093619f664f7555dbe60bf6
nijuyonkadesu <24ksriramv@gmail.com>
2025-04-13 22:26:33 +0530

    fix: markdown formatter

--- after/plugin/formatter.lua
+++ after/plugin/formatter.lua
@@ -1,11 +1,9 @@
 require("conform").setup({
     formatters_by_ft = {
         lua = { "stylua" },
-        -- Conform will run multiple formatters sequentially
-        python = { "black", "isort" },
-        -- You can customize some of the format options for the filetype (:help conform.format)
+        python = { "black" },
         rust = { "rustfmt", lsp_format = "fallback" },
-        -- Conform will run the first available formatter
         javascript = { "prettierd", "prettier", stop_after_first = true },
+        markdown = { "prettierd", "prettier", stop_after_first = true },
     },
 })
