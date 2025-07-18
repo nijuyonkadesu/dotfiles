f446efcb665f4ff9039b67790f06a95baa26c994
nijuyonkadesu <85601344+nijuyonkadesu@users.noreply.github.com>
2025-07-18 13:33:18 +0000

    fix: workaround for getcwd() behaviour
    but harpoon is still broken

--- config/nvim/after/plugin/telescope.lua
+++ config/nvim/after/plugin/telescope.lua
@@ -1,25 +1,25 @@
-require('telescope').setup { 
-  defaults = { 
-    file_ignore_patterns = { 
-      "node_modules", 
-      "venv", 
+require('telescope').setup {
+    defaults = {
+        file_ignore_patterns = {
+            "node_modules",
+            "venv",
+        }
     }
-  }
 }
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+local cwd = vim.fn.getcwd(-1)
+vim.keymap.set('n', '<leader>pf', function() builtin.find_files({ cwd = cwd }) end)
+vim.keymap.set('n', '<C-p>', function() builtin.git_files({ cwd = cwd }) end)
 vim.keymap.set('n', '<leader>ps', function()
-    builtin.grep_string({ search = vim.fn.input("Grep > ") });
-
+    builtin.grep_string({ search = vim.fn.input("Grep > "), cwd = cwd });
 end)
 vim.keymap.set('n', '<leader>pws', function()
     local word = vim.fn.expand("<cword>")
-    builtin.grep_string({ search = word })
+    builtin.grep_string({ search = word, cwd = cwd })
 end)
 vim.keymap.set('n', '<leader>pWs', function()
     local word = vim.fn.expand("<cWORD>")
-    builtin.grep_string({ search = word })
+    builtin.grep_string({ search = word, cwd = cwd })
 end)
 vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
 -- pf > all project file search
