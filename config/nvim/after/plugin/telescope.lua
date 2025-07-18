2391686e5f5cc445c4487523112de43b6d08e7ed
nijuyonkadesu <85601344+nijuyonkadesu@users.noreply.github.com>
2025-07-18 16:28:11 +0000

    Revert "fix: workaround for getcwd() behaviour"
    This reverts commit f446efcb665f4ff9039b67790f06a95baa26c994.

--- config/nvim/after/plugin/telescope.lua
+++ config/nvim/after/plugin/telescope.lua
@@ -1,25 +1,25 @@
-require('telescope').setup {
-    defaults = {
-        file_ignore_patterns = {
-            "node_modules",
-            "venv",
-        }
+require('telescope').setup { 
+  defaults = { 
+    file_ignore_patterns = { 
+      "node_modules", 
+      "venv", 
     }
+  }
 }
 local builtin = require('telescope.builtin')
-local cwd = vim.fn.getcwd(-1)
-vim.keymap.set('n', '<leader>pf', function() builtin.find_files({ cwd = cwd }) end)
-vim.keymap.set('n', '<C-p>', function() builtin.git_files({ cwd = cwd }) end)
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
-    builtin.grep_string({ search = vim.fn.input("Grep > "), cwd = cwd });
+    builtin.grep_string({ search = vim.fn.input("Grep > ") });
+
 end)
 vim.keymap.set('n', '<leader>pws', function()
     local word = vim.fn.expand("<cword>")
-    builtin.grep_string({ search = word, cwd = cwd })
+    builtin.grep_string({ search = word })
 end)
 vim.keymap.set('n', '<leader>pWs', function()
     local word = vim.fn.expand("<cWORD>")
-    builtin.grep_string({ search = word, cwd = cwd })
+    builtin.grep_string({ search = word })
 end)
 vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
 -- pf > all project file search
