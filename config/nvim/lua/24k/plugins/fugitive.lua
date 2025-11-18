78ada72b9e77185877968f3258f2545b76f6089f
nijuyonkadesu <85601344+nijuyonkadesu@users.noreply.github.com>
2025-11-18 04:40:01 +0000

    feat: go - copy url, gh - file history, gb - blame, gp - pull rebase gl - git graph

--- config/nvim/lua/24k/plugins/fugitive.lua
+++ config/nvim/lua/24k/plugins/fugitive.lua
@@ -1,8 +1,34 @@
 return {
-    'tpope/vim-fugitive',
+    {
+        "tpope/vim-fugitive",
 
-    config = function()
-        -- git status
-        vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
-    end
+        config = function()
+            -- git status
+            vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+            vim.keymap.set("n", "<leader>gh", ":0Gclog<CR>", { desc = "Git file history" })
+            vim.keymap.set("n", "<leader>gb", ":Git blame<CR>", { desc = "Git blame" })
+            vim.keymap.set("n", "<leader>gl", ":Git log --oneline --graph<CR>", { desc = "Git log" })
+
+            vim.keymap.set("n", "<leader>gp", function()
+                local branch = vim.fn.FugitiveHead()
+
+                if branch == "" or branch == "HEAD" then
+                    vim.notify("Could not determine current branch.", vim.log.levels.ERROR)
+                    return
+                end
+
+                local cmd = ":G pull origin " .. branch .. " --rebase"
+                vim.fn.feedkeys(cmd, "n")
+            end, { desc = "Populate :G pull origin <branch> --rebase" })
+        end
+    },
+    {
+        "tpope/vim-rhubarb",
+        dependencies = {
+            "tpope/vim-fugitive",
+        },
+        setup = function()
+            vim.keymap.set("n", "<leader>go", ":GBrowse!<CR>", { desc = "Yank github url" })
+        end
+    }
 }
