63b050ab2f758bf13742f12a4ee33237b7650323
nijuyonkadesu <85601344+nijuyonkadesu@users.noreply.github.com>
2026-03-31 15:25:19 +0000

    add fzf-native, for fun

--- config/nvim/lua/24k/plugins/telescope.lua
+++ config/nvim/lua/24k/plugins/telescope.lua
@@ -13,6 +13,7 @@ return {
                 }
             }
         }
+        require('telescope').load_extension('fzf')
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
